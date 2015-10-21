0dfdb3c0d4f8fc591966dc7c30f595855263e3eb
root <root@beaglebone.(none)>
2015-10-21 22:09:35 +0000

    Adding Bulb code Once you've changed your Firebase app name, simply `npm install` in Bulb/ and then `node server.js` to start.

---
+++ Bulb/server.js
@@ -0,0 +1,26 @@
+var Firebase = require('firebase'),
+    i2c = require('i2c');
+
+var port = '/dev/i2c-1',
+    address = 0x09;
+
+var wire = new i2c(0x09, {device: port}); // Connect to RGB LED
+wire.writeBytes(0x6f,[/* empty array */], i2cCallback); // Turn off LED script
+
+/* Replace this with the name of your firebase */
+var ref = new Firebase('https://<YOUR-FIREBASE-APP>.firebaseio.com');
+ref.limitToLast(10).on('child_added', function(snapshot) {
+  var color = snapshot.val();
+  writeRGB(color.r | 0, color.g | 0, color.b | 0);
+});
+
+function writeRGB(red, green, blue) {
+  wire.writeBytes(0x6e, [red, green, blue], i2cCallback);  
+  console.log("R: " + red + " G: " + green + " B: " + blue);
+}
+
+function i2cCallback(error) {
+  if (error) {
+    console.log(error);
+  }
+}
